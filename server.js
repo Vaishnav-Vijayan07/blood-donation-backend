9a1baea4dffce228287a1fda5c13a01fa97e3282
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-04-21 10:42:58 +0530

    removed force

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const userRoutes = require("./routes/user_routes");
 const officeRoutes = require("./routes/office_routes");
 const Admin = require("./models/admin");
 const bcrypt = require("bcrypt");
-const path = require('path');
-
+const path = require("path");
 
 require("dotenv").config();
 
@@ -27,7 +26,7 @@ if (!process.env.ADMIN_PASSWORD) {
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Debug middleware to log incoming requests
 app.use((req, res, next) => {
@@ -60,7 +59,7 @@ const initializeAdmin = async () => {
 
 // Database synchronization and server start
 sequelize
-  .sync({ force: true })
+  .sync()
   .then(async () => {
     await initializeAdmin();
     const port = process.env.PORT || 7700;
